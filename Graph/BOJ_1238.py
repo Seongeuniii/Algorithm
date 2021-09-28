7efecddd870cdba0545426b0adae45207e61d465
Seongeuniii <kse8538@naver.com>
2021-09-28 18:07:26 +0900

    fix code

--- Graph/BOJ_1238.py
+++ Graph/BOJ_1238.py
@@ -1,30 +1,27 @@
 import sys,math
-from heapq import heappop,heappush
+from heapq import heappop,heappush, merge
 N,M,X = map(int,sys.stdin.readline().split())
-graph = [[] for _ in range(N+1)] 
+graph = [[] for _ in range(N+1)]
+graph_reverse = [[] for _ in range(N+1)]
 for _ in range(M):
     a,b,t = map(int,sys.stdin.readline().split())
     graph[a].append((t,b))
+    graph_reverse[b].append((t,a))
 heap = []
-party = []
-def dijkstra(start):
-    global party
+def dijkstra(start,G):
     result = [math.inf for _ in range(N+1)]
     heappush(heap,(0,start))
     result[start] = 0
     while heap:
         value, node = heappop(heap)
-        for v,nd in graph[node]:
+        for v,nd in G[node]:
             if result[nd] > value + v:
                 result[nd] = value + v
                 heappush(heap,(result[nd],nd))
-    if start == X:
-        party = result
-    return result[X]
-li = [0]
+    return result
+dep = dijkstra(X,graph_reverse)
+end = dijkstra(X,graph)
+a = 0
 for i in range(1,N+1):
-    a = dijkstra(i)
-    li.append(a)
-for j in range(1,N+1):
-    li[j] = li[j]+party[j]
-print(max(li))
+    a = max(a,dep[i]+end[i])
+print(a)
