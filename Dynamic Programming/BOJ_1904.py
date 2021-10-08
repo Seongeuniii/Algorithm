d0483c7e2ee43063c2c3b98760460bc448706e69
Seongeuniii <kse8538@naver.com>
2021-10-08 21:27:18 +0900

    Dynamic programming

--- Dynamic Programming/BOJ_1904.py
+++ Dynamic Programming/BOJ_1904.py
@@ -1,2 +1,8 @@
 import sys
-print(sys.getsizeof(999999999999999999999999999999999999999999999999999999999999999999999999999999999999))
+N = int(sys.stdin.readline())
+dp = [0 for _ in range(N+1)]
+dp[0] = 1
+dp[1] = 1
+for i in range(2,N+1):
+    dp[i] = (dp[i-1]+dp[i-2])%15746
+print(dp[N])
