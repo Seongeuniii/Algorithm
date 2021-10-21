95cbe34b1ade4e6302eee1db19aff4d27390d42f
Seongeuniii <kse8538@naver.com>
2021-10-21 16:18:28 +0900

    Dynamic Programming

--- Dynamic Programming/BOJ_1890.py
+++ Dynamic Programming/BOJ_1890.py
@@ -16,3 +16,25 @@ for i in range(N):
       dp[i][j+board[i][j]] += dp[i][j]
 
 print(dp[N-1][N-1])
+
+N = int(input())
+board = [list(map(int,input().split())) for _ in range(N)]
+dp = [[0 for _ in range(N)] for _ in range(N)]
+dp[0][0] = 1
+
+for i in range(N):
+  for j in range(N):
+
+    idx = 1
+    while j-idx >= 0:
+      if board[i][j-idx] == idx:
+        dp[i][j] += dp[i][j-idx]
+      idx += 1
+
+    idx = 1
+    while i-idx >= 0:
+      if board[i-idx][j] == idx:
+        dp[i][j] += dp[i-idx][j]
+      idx += 1
+      
+print(dp[N-1][N-1])
