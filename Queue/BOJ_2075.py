2b5e7145d9342b396235e9dd56d0948cfe6388e7
Seongeuniii <kse8538@naver.com>
2021-11-27 20:02:35 +0900

    Queue

--- Queue/BOJ_2075.py
+++ Queue/BOJ_2075.py
@@ -8,3 +8,24 @@ for _ in range(N):
   li.sort(reverse=True)
   li = li[:N]
 print(li[-1])
+
+# 큐
+import sys
+from heapq import heappush,heappop,heapify
+input = sys.stdin.readline
+
+N = int(input())
+heap = list(map(int,input().split()))
+heapify(heap)
+
+for _ in range(N-1):
+  line = sorted(list(map(int,input().split())), reverse=True)
+  
+  for l in line:
+    if heap[0] < l:
+      heappop(heap)
+      heappush(heap, l)
+    else:
+      break
+    
+print(heappop(heap))
