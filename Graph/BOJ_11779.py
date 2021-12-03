a4659e1476361ab1f1a9afdf92a410ae197a16a7
Seongeuniii <kse8538@naver.com>
2021-12-03 17:50:39 +0900

    Dijkstra Algorithm

--- Graph/BOJ_11779.py
+++ Graph/BOJ_11779.py
@@ -5,7 +5,6 @@ N = int(input())
 M = int(input())
 
 graph = [[] for _ in range(N+1)]
-visited = [0]*(N+1)
 result = [math.inf]*(N+1)
 parent = [0]*(N+1)
 answer = []
@@ -22,13 +21,13 @@ def dijkstra(start):
   heappush(heap,(0,start))
   while heap:
     value, node = heappop(heap)
-    if not visited[node]:
-      visited[node] = 1
-      for v,nd in graph[node]:
-        if value + v < result[nd]:
-          result[nd] = value + v
-          parent[nd] = node
-          heappush(heap, (result[nd],nd))
+    if value > result[node]:
+      continue
+    for v,nd in graph[node]:
+      if value + v < result[nd]:
+        result[nd] = value + v
+        parent[nd] = node
+        heappush(heap, (result[nd],nd))
 
 def dfs(start):
   node = start
