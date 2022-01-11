acb3166c37c50de2bc0747808c7ed12bb65089ce
Seongeuniii <kse8538@naver.com>
2022-01-11 13:58:02 +0900

    Dijkstra Algorithm

--- Graph/BOJ_1486.py
+++ Graph/BOJ_1486.py
@@ -3,45 +3,49 @@ from heapq import heappop, heappush
 input = sys.stdin.readline
 
 N,M,T,D = map(int,input().split())
-board = [list(map(ord,input().strip())) for _ in range(N)]
-
-for n in range(N):
-  for m in range(M):
-    if board[n][m] > 96: board[n][m] -= 71
-    else: board[n][m] -= 65
+board = [[i-65 if i < 97 else i-71 for i in list(map(ord,input().strip()))] for _ in range(N)]
 
 def dijkstra(sx, sy):
   dx, dy = [0,0,1,-1], [1,-1,0,0]
 
-  time = [[math.inf]*M for _ in range(N)]
+  up_time = [[math.inf]*M for _ in range(N)]
+  down_time = [[math.inf]*M for _ in range(N)]
   heap = []
 
-  time[sx][sy] = 0
-  heappush(heap, (0,sx,sy))
+  up_time[sx][sy] = 0
+  down_time[sx][sy] = 0
+  heappush(heap, (0,1,sx,sy)) # up은 1 down은 0
+  heappush(heap, (0,0,sx,sy))
 
   while heap:
-    c,x,y = heappop(heap)
+    c,ud,x,y = heappop(heap)
     for i in range(4):
       nx, ny = x+dx[i], y+dy[i]
       if 0<=nx<N and 0<=ny<M:
         diff = abs(board[x][y]-board[nx][ny])
+        if ud:
+          if diff <= T:
+            if board[nx][ny] > board[x][y]: # 더 높은 곳
+              if up_time[nx][ny] > c + diff**2:
+                up_time[nx][ny] = c + diff**2
+                heappush(heap, (c+diff**2, ud, nx, ny))
+            elif up_time[nx][ny] > c + 1:
+              up_time[nx][ny] = c + 1
+              heappush(heap, (c+1, ud, nx, ny))
+        else:
+          if diff <= T:
+            if board[nx][ny] < board[x][y]:
+              if down_time[nx][ny] > c + diff**2:
+                down_time[nx][ny] = c + diff**2
+                heappush(heap, (c+diff**2, ud, nx, ny))
+            elif down_time[nx][ny] > c + 1:
+              down_time[nx][ny] = c + 1
+              heappush(heap, (c+1, ud, nx, ny))
+  answer = 0
+  for n in range(N):
+    for m in range(M):
+      if up_time[n][m] + down_time[n][m] <= D:
+        answer = max(answer, board[n][m])
+  return answer
 
-        if diff <= T:
-          if board[nx][ny] > board[x][y]: # 더 높은 곳
-            if time[nx][ny] > c + diff**2:
-              time[nx][ny] = c + diff**2
-              heappush(heap, (c+diff**2, nx, ny))
-          elif time[nx][ny] > c + 1:
-            time[nx][ny] = c + 1
-            heappush(heap, (c+1, nx, ny))
-  
-  return time
-
-time = dijkstra(0,0)
-answer = 0
-for i in range(N):
-  for j in range(M):
-    t = dijkstra(i,j)
-    if time[i][j] + t[0][0] <= D:
-      answer = max(answer, board[i][j])
-print(answer)
+print(dijkstra(0,0))
