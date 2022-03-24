5a4f2f2eaa1bb6becea701476eb3f0b2e9523d42
Seongeuniii <kse8538@naver.com>
2022-03-24 23:42:47 +0900

    Algorithm

---
+++ Programmers/L1_음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i, a in enumerate(absolutes):
+        if signs[i]:
+            answer += a
+        else:
+            answer -= a        
+        
+    return answer
